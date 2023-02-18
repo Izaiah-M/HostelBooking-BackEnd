19dd721982440888b16e3c196989bacd9706cf59
iz.aia.h <imukisa024@gmail.com>
2023-02-18 12:22:27 +0300

    Can now add data to the db

--- src/JDBC_conector/Driver.java
+++ src/JDBC_conector/Driver.java
@@ -1,31 +1,33 @@
 package JDBC_conector;
 
 import java.sql.*;
+import java.util.Scanner;
 // import java.util.logging.Level;
 // import java.util.logging.Logger;
 
 public class Driver {
-    public static void main(String[] args) {
+
+    private String username = "root";
+    private String password = "#Admin123";
+    Scanner scanner = new Scanner(System.in);
+
+    // Getting Info method.
+    public void gettingInfo() {
 
         System.out.println("Welcome to the Trial!!");
 
-        String username = "root";
-        String password = "#Admin123";
-
         Statement statement; // runs SQL statement
-        // String useSQL = new String("USE Hostel_Management_System"); // We are picking
-        // the db to use
-
         String output;
 
         ResultSet result; // Holds output from SQL
         String SQL = "SELECT * FROM residents";
 
         try {
+
+            // Establishing the connection
             Class.forName("com.mysql.cj.jdbc.Driver");
             String dbURL = "jdbc:mysql://localhost:3306/hostel_management_system";
             Connection dbConnection = DriverManager.getConnection(dbURL, username, password);
-
             statement = dbConnection.createStatement(); // Allows SQL to be executed
 
             // Running the query
@@ -43,5 +45,44 @@ public class Driver {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public void insertHostel() {
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String dbURL = "jdbc:mysql://localhost:3306/hostel_management_system";
+            Connection dbConnection = DriverManager.getConnection(dbURL, username, password);
+            PreparedStatement prstmt;
+
+            System.out.println("Enter name of Hostel: ");
+            String name = scanner.nextLine();
+            System.out.println("Enter Hostel location: ");
+            String location = scanner.nextLine();
+
+            String query = "INSERT INTO hostel_table (hostel_name, hostel_location, manager_id) VALUES (?, ?, NULL)";
+
+            // Adding the stuff to the DB
+            prstmt = dbConnection.prepareStatement(query);
+
+            prstmt.setString(1, name);
+            prstmt.setString(2, location);
+
+            int rowsAffected = prstmt.executeUpdate();
+            System.out.println(rowsAffected + " row(s) affected.");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Driver trial = new Driver();
+
+        trial.insertHostel();
     }
 }
