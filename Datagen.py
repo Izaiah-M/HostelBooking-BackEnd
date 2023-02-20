d7c4e6b09c07c161c48ff44616ac0a9a2afc1414
iz.aia.h <imukisa024@gmail.com>
2023-02-20 13:09:46 +0300

    Added data into the tables except for the residents table

--- Datagen.py
+++ Datagen.py
@@ -5,7 +5,7 @@ import random
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="#Admin123",
+  password="",
   database="hostel_management_system"
 )
 
@@ -53,14 +53,63 @@ def generate_random_email():
     
     return ''.join(random_email) + "@mail.com"
 
+def generate_random_types():
+    types = ["Double", "Single", "Double self contained", "Single self contained"]
+
+    chosen = random.choice(types)
+
+    return chosen
+
+def generate_random_status():
+    status = ["Booked", "Vacant", "Under maintenance"]
+
+    chosen = random.choice(status)
+
+    return chosen
+
 
 # Inserting the data into the database.
+
 def datagen():
-    for i in range(0, 10):
+    for i in range(0, 40):
+        room_type = generate_random_types()
+        room_status = generate_random_status()
+        # price = generate_random_price()
+
+        mycursor.execute("INSERT INTO rooms (hostel_id, room_type, room_status) VALUES (9, %s, %s);", (room_type, room_status))
+
+        # For generating rooms.
+        # mycursor.execute("INSERT INTO rooms (hostel_id, room_type, room_status) VALUES (9, %s, %s);", (room_type, room_status))
+        # mycursor.execute("INSERT INTO rooms (hostel_id, room_type, room_status) VALUES (10, %s, 'Booked');", (room_type,))
+        mydb.commit()
+
+# datagen()
+
+
+# Inserting into residents
+def generate_room(assigned_rooms):
+    # Generate a random number between 1 and 17
+    room_id = random.randint(51, 72)
+    # Keep generating new numbers until an unassigned one is found
+    while room_id in assigned_rooms:
+        room_id = random.randint(51, 72)
+    # Add the assigned room ID to the list
+    assigned_rooms.append(room_id)
+    return room_id
+
+# print(generate_room())
+
+def into_residents():
+    assigned_rooms = []
+    for i in range(0, 21):
         name = generate_random_letters()
-        password = generate_random_passwords()
         email = generate_random_email()
+        password = generate_random_passwords()
+        room_id = generate_room(assigned_rooms)
 
-        mycursor.execute("INSERT INTO managers (manager_name, manager_email, manager_password) VALUES (?, ?, ?);", name, email, password)
+        mycursor.execute("INSERT INTO residents (resident_name, resident_email, resident_password, room_id, hostel_id) VALUES (%s, %s, %s, %s, 1);", (name, email, password, room_id))
+        mydb.commit()
 
-datagen()
+into_residents()
+
+
--- bin/JDBC_conector/Queries.sql
+++ bin/JDBC_conector/Queries.sql
@@ -9,7 +9,7 @@ CREATE TABLE hostels (
     hostel_location VARCHAR(30),
     phone INT,
     num_rooms INT,
-    hostel_status TEXT,
+    hostel_status VARCHAR(30),
     mgr_id INT
 );
 
@@ -32,7 +32,7 @@ CREATE TABLE reservations (
 );
 
 CREATE TABLE managers (
-  manager_id INT PRIMARY KEY,
+  manager_id INT PRIMARY KEY AUTO_INCREMENT,
   manager_name VARCHAR(20),
   manager_email VARCHAR(20),
   manager_password VARCHAR(30),
@@ -40,7 +40,7 @@ CREATE TABLE managers (
 );
 
 CREATE TABLE residents (
-  resident_id int PRIMARY KEY,
+  resident_id int PRIMARY KEY AUTO_INCREMENT,
   resident_name VARCHAR(20),
   resident_email VARCHAR(20),
   resident_password VARCHAR(30),
@@ -51,8 +51,10 @@ CREATE TABLE residents (
 );
 
 CREATE TABLE hostels_managers (
-  mgr_id INT
+  mgr_id INT UNIQUE
 );
+
+
 
 ALTER TABLE hostels ADD FOREIGN KEY (mgr_id) REFERENCES hostels_managers (mgr_id);
 
@@ -69,3 +71,49 @@ ALTER TABLE residents ADD FOREIGN KEY (room_id) REFERENCES rooms (room_id);
 ALTER TABLE residents ADD FOREIGN KEY (hostel_id) REFERENCES hostels (hostel_id);
 
 ALTER TABLE hostels_managers ADD FOREIGN KEY (mgr_id) REFERENCES managers (manager_id);
+
+SELECT * FROM managers;
+
+DELETE FROM hostels;
+
+-- To reset the PRIMARY key AUTO_INCREMENT
+ALTER TABLE residents AUTO_INCREMENT = 1;
+
+INSERT INTO hostels_managers 
+VALUES
+(1), (2), (3), (4), (5), (6), (7), (8), (9), (10);
+
+INSERT INTO hostels
+(hostel_name, hostel_location, phone, num_rooms, hostel_status, mgr_id)
+VALUES
+("David's Ark", "Buguju", 0757106452, 50, "Vacancies", 1),
+("Sky hostel", "Buguju", 0727176602, 60, "Vacancies", 2),
+("Dandeyy Girls", "Kataba", 0778495752, 50, "Vacancies", 3),
+("HighField Hostel", "Buguju", 0800288838, 70, "Vacancies", 4),
+("Chronos Hostel", "Wandegeya", 0800445622, 55, "Vacancies", 5),
+("Carlton Hostel", "Buguju", 0788927374, 30, "Fully-booked", 6),
+("St. Micheal Hostels", "Buguju", 0712425366, 50, "Vacancies", 7),
+("New Boys Hostel", "Wandegeya", 0772990101, 30, "Fully-booked", 8),
+("David's Ark", "Bishop", 0703445772, 74, "Vacancies", 9),
+("Premium Hostel", "Buguju", 0743566278, 40, "Fully-booked", 10);
+
+UPDATE managers
+SET hostel_id = 10
+WHERE manager_id = 10;
+
+-- DELETE FROM rooms;
+
+SELECT * FROM rooms;
+
+UPDATE hostels
+SET hostel_name = "Freshers Banquet"
+WHERE hostel_id = 9;
+
+SELECT room_id, room_type, room_status FROM rooms
+WHERE room_status = "Booked" AND hostel_id = 1;
+
+-- 2....21, 60
+
+SELECT * FROM hostels;
+
+DELETE FROM residents
